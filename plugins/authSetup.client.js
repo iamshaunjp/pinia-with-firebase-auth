2f1bc3ff120aca3c9b871eddb575582e17df831a
iamshaunjp <shaun@thenetninja.co.uk>
2024-09-01 13:50:46 +0100

    lesson-9

--- plugins/authSetup.client.js
+++ plugins/authSetup.client.js
@@ -1,9 +1,34 @@
 import { useAuthStore } from '@/stores/auth'
 
+const PROTECTED_ROUTES = ['/']
+const GUEST_ONLY_ROUTES = ['/login', '/signup']
+
 export default defineNuxtPlugin(() => {
   const authStore = useAuthStore()
+  const router = useRouter()
 
   // setup the auth listener
   authStore.setupAuthListener()
+
+  const redirectUser = () => {
+    const { path } = router.currentRoute.value
+
+    // redirect authenticated users to homepage
+    if (authStore.user) {
+      if (GUEST_ONLY_ROUTES.includes(path)) {
+        router.push('/')
+      }
+    }
+
+    // redirect unauthentiacted users to login
+    if (!authStore.user) {
+      if (PROTECTED_ROUTES.includes(path)) {
+        router.push('/login')
+      }
+    }
+  }
+
+  // watch user and redirect when it changes
+  watch(() => authStore.user, () => redirectUser())
 
 })
